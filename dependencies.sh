31b5368f95c7925bf41ca3396d53f7580f9ed420
howls <mfloriach90@gmail.com>
2017-04-27 18:27:50 +0900

    add confirmation

--- dependencies.sh
+++ dependencies.sh
@@ -11,7 +11,7 @@ sudo apt-get update
 sudo apt-get install python-pip -y
 sudo apt install influxdb-client -y
 sudo apt-get install libpq-dev python-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev libffi-dev -y
-#sudo apt-get install rabbitmq-server -y
+sudo apt-get install rabbitmq-server -y
 
 #Configure RabbitMQ
 sudo rabbitmqctl add_user oocran oocran
@@ -19,8 +19,8 @@ sudo rabbitmqctl add_vhost oocran
 sudo rabbitmqctl set_permissions -p oocran oocran ".*" ".*" ".*"
 
 #InfluxDB
-#sudo apt-get install influxdb
-#influx  -execute "CREATE USER admin WITH PASSWORD 'oocran' WITH ALL PRIVILEGES"
+sudo apt-get install influxdb
+influx  -execute "CREATE USER admin WITH PASSWORD 'oocran' WITH ALL PRIVILEGES"
 #sudo echo deb https://packagecloud.io/grafana/stable/debian/ jessie main >> /etc/apt/sources.list
 #curl https://packagecloud.io/gpg.key | sudo apt-key add -
 #sudo apt-get update
