9b894df067547b1a5d361b36ab52c350e3b30313
howls90 <mfloriach90@gmail.com>
2017-10-16 16:32:03 +0900

    ues delete

--- examples/scripts/files/monitorVNF.py
+++ examples/scripts/files/monitorVNF.py
@@ -2,24 +2,61 @@
 
 import requests
 from time import sleep
+import time
 import psutil
+import os
 
-NVF = 'RX'
-IP = '147.83.118.227'
-DB = 'rx'
-USER = 'admin'
-PASSWORD = 'admin'
+NVF = 'test'
+IP = '192.168.1.117'
+DB = 'ns_1'
+USER = 'test'
+PASSWORD = 'test'
 TIME = 1
+interface = "lo"
 
+cpu = 0.0
+ul=0.00
+dl=0.00
+t0 = time.time()
+upload=psutil.net_io_counters(pernic=True)[interface][0]
+download=psutil.net_io_counters(pernic=True)[interface][1]
+up_down=(upload,download)
 
 while True:
+    for proc in psutil.process_iter():
+        process = psutil.Process(proc.pid)
+        pname = process.name()
+        if pname == "pdsch_enodeb" or pname == "pdsch_ue":
+            percent = process.cpu_percent(interval=0.1)
+            if str(percent) != '0.0':
+                cpu = 'cpu' + ' value=%s' % percent
+                disk = 'disk' + ' value=%s' % psutil.disk_usage('/').percent
+                ram = 'ram' + ' value=%s' % round(process.memory_percent(), 2)
 
-    cpu = 'cpu' + ' value=%s' % psutil.cpu_percent()
-    disk = 'disk' + ' value=%s' % psutil.disk_usage('/').percent
-    ram = 'ram' + ' value=%s' % psutil.virtual_memory().used
+                last_up_down = up_down
+                upload = psutil.net_io_counters(pernic=True)[interface][0]
+                download = psutil.net_io_counters(pernic=True)[interface][1]
+                t1 = time.time()
+                up_down = (upload, download)
+                try:
+                    ul, dl = [(now - last) / (t1 - t0) / 1024.0
+                              for now, last in zip(up_down, last_up_down)]
+                    t0 = time.time()
+                except:
+                    pass
+                if dl > 0.1 or ul >= 0.1:
+                    time.sleep(0.75)
+                    os.system('cls')
+                    #print('UL: {:0.2f} kB/s \n'.format(ul) + 'DL: {:0.2f} kB/s'.format(dl))
 
-    requests.post("http://%s:8086/write?db=%s" % (IP, DB), auth=(USER, PASSWORD), data=cpu)
-    requests.post("http://%s:8086/write?db=%s" % (IP, DB), auth=(USER, PASSWORD), data=disk)
-    requests.post("http://%s:8086/write?db=%s" % (IP, DB), auth=(USER, PASSWORD), data=ram)
+                network_in = 'network_in_' + NVF + ' value=%s' % ul
+                network_out = 'network_out_' + NVF + ' value=%s' % dl
+
+                requests.post("http://%s:8086/write?db=%s" % (IP, DB), auth=(USER, PASSWORD), data=cpu)
+                requests.post("http://%s:8086/write?db=%s" % (IP, DB), auth=(USER, PASSWORD), data=disk)
+                requests.post("http://%s:8086/write?db=%s" % (IP, DB), auth=(USER, PASSWORD), data=ram)
+                requests.post("http://%s:8086/write?db=%s" % (IP, DB), auth=(USER, PASSWORD), data=network_in)
+                requests.post("http://%s:8086/write?db=%s" % (IP, DB), auth=(USER, PASSWORD), data=network_out)
 
     sleep(TIME)
+
--- install/dependencies.sh
+++ install/dependencies.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
-echo "Install OOCRAN dependencies!"
-echo ""
-cd /tmp
-#sudo apt-get update
 sudo apt-get install python-pip -y
 sudo apt install influxdb-client -y
 sudo apt-get install libpq-dev python-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev libffi-dev -y
 sudo apt-get install rabbitmq-server -y
-sudo apt-get install libssl-dev libcurl4-openssl-dev python-dev -y
+sudo apt-get install libssl-dev libcurl4-openssl-dev -y
 sudo apt-get install nfs-kernel-server -y
 #InfluxDB
 sudo apt-get install influxdb
 #Vagrant
 sudo apt-get build-dep vagrant ruby-libvirt -y
-sudo apt-get install qemu libvirt-bin ebtables dnsmasq -y
 sudo apt-get install libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev -y
-sudo apt-get install vagrant -y
+#Config
+influx  -execute "CREATE USER admin WITH PASSWORD 'admin' WITH ALL PRIVILEGES"
+sudo rabbitmqctl set_permissions -p oocran oocran ".*" ".*" ".*"
+vagrant plugin install vagrant-openstack-provider
+vagrant plugin install vagrant-libvirt
+vagrant plugin install vagrant-vbguest
+sudo rabbitmqctl add_user oocran oocran
+sudo rabbitmqctl add_vhost oocran
