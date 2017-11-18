c3f3d83792e8700c0230e4cf31891356090afcfe
howls90 <mfloriach90@gmail.com>
2017-11-18 16:15:59 +0900

    setuptool package dependencies

--- oocran/django/oocran/settings.py
+++ oocran/django/oocran/settings.py
@@ -83,21 +83,21 @@ WSGI_APPLICATION = 'oocran.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
 DATABASES = {
-'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'oocran',
-    'HOST': '127.0.0.1',
-    'PORT': '3306',
-    'USER': 'oocran',
-    'PASSWORD': 'oocran',
-}}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+#DATABASES = {
+#'default': {
+#    'ENGINE': 'django.db.backends.mysql',
+#    'NAME': 'oocran',
+#    'HOST': '127.0.0.1',
+#    'PORT': '3306',
+#    'USER': 'oocran',
+#    'PASSWORD': 'oocran',
+#}}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
