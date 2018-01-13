5282850c00abaed5d30a8fa951d69616de18569f
howls90 <mfloriach90@gmail.com>
2018-01-13 16:40:44 +0900

    Add doc string to BBU and drivers

--- oocran/django/bbus/urls.py
+++ oocran/django/bbus/urls.py
@@ -29,9 +29,9 @@ from .views import (
 urlpatterns = [
     url(r'^(?P<id>\d+)/details/$', details, name='details'),
     url(r'^(?P<id>\d+)/alert/$', alert, name='alert'),
-    url(r'^(?P<id>\d+)/ue/$', ue, name='ue'),
+    #url(r'^(?P<id>\d+)/ue/$', ue, name='ue'),
     url(r'^(?P<id>\d+)/scheduler/$', scheduler, name='scheduler'),
-    url(r'^(?P<id>\d+)/log/$', log, name='log'),
-    url(r'^(?P<id>\d+)/console/$', console, name='console'),
+    #url(r'^(?P<id>\d+)/log/$', log, name='log'),
+    #url(r'^(?P<id>\d+)/console/$', console, name='console'),
     url(r'^$', list, name="list"),
 ]
